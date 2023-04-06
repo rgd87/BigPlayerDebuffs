6a6f4e0dcb8c822543346fdf2fdee986f537e7fd
oorzkws <65210810+oorzkws@users.noreply.github.com>
2023-04-06 05:58:28 -0600

    Config update - focus target support, cleaner UI

--- BigPlayerDebuffsConfig.cs
+++ BigPlayerDebuffsConfig.cs
@@ -2,10 +2,7 @@
 using Dalamud.Plugin;
 using ImGuiNET;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Numerics;
-using Dalamud.Game.Text;
 
 namespace BigPlayerDebuffs
 {
@@ -27,8 +24,11 @@ namespace BigPlayerDebuffs
 
         public int Version { get; set; }
 
+        // What does the 'b' mean?
         public float bScale = 1.4f;
-
+        public float fScale = 1.25f;
+        public bool includeMainTarget = true;
+        public bool includeFocusTarget = true;
 
         public void Init(BigPlayerDebuffs plugin, DalamudPluginInterface pluginInterface) {
             this.plugin = plugin;
@@ -47,17 +47,36 @@ namespace BigPlayerDebuffs
 
             var modified = false;
 
-            ImGui.SetNextWindowSize(new Vector2(500 * scale, 350), ImGuiCond.FirstUseEver);
-            ImGui.SetNextWindowSizeConstraints(new Vector2(500 * scale, 350), new Vector2(560 * scale, 650));
-            ImGui.Begin($"{plugin.Name} Config", ref drawConfig, ImGuiWindowFlags.NoCollapse);
-
-            modified |= ImGui.SliderFloat("Own Buff/Debuff Scale", ref bScale, 1.0F, 2.0F);
-
+            ImGui.SetNextWindowSize(new Vector2(550, 120) * scale, ImGuiCond.FirstUseEver);
+            ImGui.SetNextWindowSizeConstraints(new Vector2(550, 110), new Vector2(1100, 650) * scale);
+            ImGui.Begin($"{plugin.Name} Configuration", ref drawConfig, ImGuiWindowFlags.NoCollapse);
+            // Target UI
+            ImGui.BeginGroup();
+            modified |= ImGui.Checkbox("##Enable scaling in Target UI",ref includeMainTarget);
+            if(ImGui.IsItemHovered())
+                ImGui.SetTooltip("Enable scaling in target UI");
+            ImGui.SameLine();
+            ImGui.BeginDisabled(!includeMainTarget);
+            modified |= ImGui.SliderFloat("Scale in Target UI", ref bScale, 1.0F, 2.0F, "%.2f");
+            ImGui.EndDisabled();
+            ImGui.EndGroup();
+            // Focus target
+            ImGui.BeginGroup();
+            modified |= ImGui.Checkbox("##Enable scaling in Focus Target UI",ref includeFocusTarget);
+            if(ImGui.IsItemHovered())
+                ImGui.SetTooltip("Enable scaling in focus target UI");
+            ImGui.SameLine();
+            ImGui.BeginDisabled(!includeFocusTarget);
+            modified |= ImGui.SliderFloat("Scale in Focus Target UI", ref fScale, 1.0F, 2.0F, "%.2f");
+            ImGui.EndDisabled();
+            ImGui.EndGroup();
+            ImGui.Text("Hint: Ctrl+Click a slider to input a number directly");
             ImGui.End();
 
 
             if (modified)
             {
+                plugin.ResetTargetStatus();
                 Save();
             }
 
